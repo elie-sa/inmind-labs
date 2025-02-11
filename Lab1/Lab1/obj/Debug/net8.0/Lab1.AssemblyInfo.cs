1d1a63636d07feb8bb4e20564c789ecc0d1e3009
elie-sa <e.sawmaawad@gmail.com>
2025-02-11 20:54:06 +0200

    Add changes before checkout

--- Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ea46ed250e2dcfc491d0877af423d32cecdce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8adc4fad55abb74496a046908716135997d78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
